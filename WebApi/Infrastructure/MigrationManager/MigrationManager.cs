6bf4410d1ce49f3960e58dd055eaa6876c73d345
adchouaib <eladraoui.chouaib@gmail.com>
2023-08-09 00:54:37 +0100

    docker compose

--- WebApi/Infrastructure/MigrationManager/MigrationManager.cs
+++ WebApi/Infrastructure/MigrationManager/MigrationManager.cs
@@ -2,33 +2,45 @@ using Infrastructure.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Threading;
 
 namespace Infrastructure.MigrationManager
 {
     public static class MigrationManager
     {
-        private static int _numberOfRetries;
+        private const int MaxRetries = 6;
+        private const int RetryDelayMilliseconds = 10000;
+
         public static IHost MigrateDatabase(this IHost host)
         {
             using (var scope = host.Services.CreateScope())
             {
                 using var appContext = scope.ServiceProvider.GetRequiredService<DataContext>();
-                try
+                bool migrationSucceeded = false;
+                int retryCount = 0;
+
+                while (!migrationSucceeded && retryCount < MaxRetries)
                 {
-                    appContext.Database.Migrate();
-                }
-                catch (Exception)
-                {
-                    if (_numberOfRetries < 6)
+                    try
                     {
-                        Thread.Sleep(10000);
-                        _numberOfRetries++;
-                        Console.WriteLine($"The server was not found or was not accessible. Retrying... #{_numberOfRetries}");
-                        MigrateDatabase(host);
+                        appContext.Database.Migrate();
+                        migrationSucceeded = true;
                     }
-                    throw;
+                    catch (Exception)
+                    {
+                        retryCount++;
+                        Console.WriteLine($"Migration failed. Retrying... Attempt {retryCount}/{MaxRetries}");
+                        Thread.Sleep(RetryDelayMilliseconds);
+                    }
+                }
+
+                if (!migrationSucceeded)
+                {
+                    throw new ApplicationException("Migration failed after multiple retries.");
                 }
             }
+
             return host;
         }
     }
